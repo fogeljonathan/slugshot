0f6c3bb3122257da27baccb13bc5a445920f09d1
Jonathan Fogel <fogeljonathan@gmail.com>
2026-02-20 23:07:36 -0500

    added camera, added flicker to lights!

---
+++ scenes/combat/_lamp.gd
@@ -0,0 +1,10 @@
+extends Node2D
+
+@onready var last_mod_time_ms:int = Time.get_ticks_msec()
+var update_rate_ms = 100
+
+func _process(delta: float) -> void:
+	if Time.get_ticks_msec() - last_mod_time_ms > update_rate_ms :
+		last_mod_time_ms = Time.get_ticks_msec()
+		var mod:float = randf_range(-2,2) * delta
+		$PointLight2D.energy = clampf($PointLight2D.energy +mod, 0,.5)
